21aad79ecca8af7c4ed3afcfd2d422bdc8adf3ef
Aureo151 <aureotzic@gmail.com>
2025-05-22 08:27:39 -0600

    creacion de clases

---
+++ Models/Pelicula.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TercerParcial.Models
+{
+    public class Pelicula:Media
+    {
+        [Required]
+        public int Duracion { get; set; }
+        [Required]
+        public string Idioma { get; set; }
+    }
+}
---
+++ Models/Videojuego.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TercerParcial.Models
+{
+    public class Videojuego:Media
+    {
+        [Required]
+        public string Plataforma { get; set; }
+        [Required]
+        public int Peso { get; set; }
+    }
+}
